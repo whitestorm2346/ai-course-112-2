bcb1979eb5cc753694cdb81f96205bf951724f97
whitestorm2346 <andrew770426@gmail.com>
2024-03-03 02:42:23 +0800

    Finished the first homework about linear regression

--- regression/main.py
+++ regression/main.py
@@ -1,43 +1,59 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
 from sklearn.feature_selection import f_regression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-import statsmodels.api as sm
 from sklearn.model_selection import cross_val_predict
 
 
 df = pd.read_csv('regression_exercise.csv')
 
 # X = df[['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']]
-# X = df[['A', 'C', 'D', 'E', 'F', 'G', 'H']]
-# X = df[['A', 'C', 'D', 'E', 'F', 'H']]
-# X = df[['A', 'C', 'E', 'F', 'H']]
-X = df[['A', 'C', 'E', 'F']]
+# X = df[['A', 'B', 'D', 'E', 'F', 'G', 'H']]
+# X = df[['A', 'B', 'E', 'F', 'G', 'H']]
+# X = df[['A', 'B', 'F', 'G', 'H']]
+# X = df[['A', 'B', 'F', 'G']]
+# X = df[['A', 'B', 'F']]
+# X = df[['A', 'B']]
+X = df[['A']]
 y = df['Y']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
+# ============= 挑選顯著因子 =============
+counts = [0] * len(X.columns)
+
+for t in range(500):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+
+    F_values, p_values = f_regression(X_train, y_train)
+
+    # for i in range(len(F_values)):
+    #     print(X.columns[i], ':')
+    #     print('f = %f' % F_values[i])
+    #     print('p = %f' % p_values[i])
+    #     print()
+
+    idx_p_max = np.argmax(p_values)
+    counts[idx_p_max] += 1
+
+for i in range(len(counts)):
+    print(X.columns[i], ': ', counts[i])
+# =======================================
+
+
+# ============= 模型訓練 =================
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# 在測試集上進行預測
+print('y = %f + %f*A' % (model.intercept_, model.coef_[0]), end='\n\n')
+# =======================================
+
+
+# ============= 測試模型 ==================
 y_pred = model.predict(X_test)
-
-# 添加截距項
-X = sm.add_constant(X)
-
-# 初始化線性回歸模型
-model = sm.OLS(y, X)
-
-# 用最小二乘法擬合模型
-results = model.fit()
-
-# 模型摘要，包括係數和 p 值
-print(results.summary())
 
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
@@ -45,27 +61,18 @@ rmse = np.sqrt(mse)
 
 print("MSE:", mse)
 print("MAE:", mae)
-print("RMSE:", rmse)
-
-coef_column = results.params
-
-print(f"regression: {coef_column[0]}", end='')
-
-for i in range(1, len(X.columns)):
-    print(f' + {coef_column[i]}*{X.columns[i]}', end='')
-
-print()
+print("RMSE:", rmse, end='\n\n')
 
 mean_y_train = y_train.mean()
 mean_y_pred = y_pred.mean()
 
 bias = mean_y_train - mean_y_pred
 
-print("Bias :", bias)
+print("Bias:", bias)
 
 # 使用 k 折交叉驗證計算模型的預測值
-y_pred_cv = cross_val_predict(LinearRegression(), X_test, y_test, cv=5)  # 假設使用 5 折交叉驗證
+y_pred_cv = cross_val_predict(model, X_test, y_test, cv=5)  # 假設使用 5 折交叉驗證
 
 variance = np.var(y_pred_cv)
 
-print("Variance :", variance)
+print("Variance:", variance)
